200ec90bae7db52af22d00800ce58f13a5c300f6
Manuel Fokam <fokammanuel1@students.wits.ac.za>
2023-07-08 22:26:45 +0200

    feature enhancement

--- start_viz_server.py
+++ start_viz_server.py
@@ -3,13 +3,13 @@ import socketio
 from aiohttp import web
 
 # create a Socket.IO server
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 
 @sio.event
-def new_frame(_, data):
-    sio.emit('on_new_frame', data=data)
+async def new_frame(_, data):
+    await sio.emit('on_new_frame', data=data)
     
 if __name__ == '__main__':
     web.run_app(app, port=sys.argv[1])
